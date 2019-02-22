2018952418d8207c4fcf0a34daeb8effe7ebcc67
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-22 14:12:43 -0800

    Add req.params to render photos by id

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use('/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
