6d8e3980a0dabcb4946d8a6410f3ce3095fc8cfa
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-22 19:15:43 -0800

    Connect proxy to client photos service

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
