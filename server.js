40489eeac78ec5a8cc183e4f2cab5459d2e521b2
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-24 16:07:38 -0800

    Final changes to photo service

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use('/:id', express.static(path.join(__dirname, 'public')));
+app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 app.listen(port, () => {
